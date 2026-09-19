af3d72fc77092446c0f803dbb70397baebdf7256
PurviDhiman1 <dhimanpurvi1@gmail.com>
2026-09-19 15:48:13 +0530

    Add inverted star pyramid pattern

---
+++ Patterns/Inverted_Star_Pyramid.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print1(int n){
+    for(int l=0; l<n; l++){
+        for(int s=0; s<l; s++){
+            cout << " ";
+        }
+
+        for(int c=0; c<2*n-(2*l+1); c++){
+            cout << "*";
+        }
+
+        for(int s=0; s<l; s++){
+            cout << " ";
+        }
+
+        cout << endl;
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    print1(n);
+}
